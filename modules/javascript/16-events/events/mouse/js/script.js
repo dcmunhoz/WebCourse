a975f34186c0758f9fdb53fd9ec269a148d69e10
Daniel Costa Munhoz <dc.munhoz@hotmail.com>
2018-04-16 23:03:19 -0300

    Aula - Javascript, Eventos Mouse

---
+++ modules/javascript/16-events/events/mouse/js/script.js
@@ -0,0 +1,26 @@
+//Eventos mouse
+
+// Click
+function clickEvent(){
+  alert('Evento Mouse Click');
+}
+
+// Double click
+function dblclickEvent(){
+  alert('Evento Double click');
+}
+
+// Mouse Down
+function omdEvent(){
+  alert('Evento Mouse Down');
+}
+
+// Mouse Over
+function omoEvent(){
+  alert('Evento Mouse Over');
+}
+
+// Mouse Out
+function omuEvent(){
+  alert('Evento Mouse Out');
+}
