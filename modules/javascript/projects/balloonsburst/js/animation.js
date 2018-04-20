d16bac8facadabcc96e3758f33c1ff13e1c47bbe
Daniel Costa Munhoz <dc.munhoz@hotmail.com>
2018-04-20 00:52:36 -0300

    Contagem regressiva finalizado, bug timer solved, balões adicionados

--- modules/javascript/projects/balloonsburst/js/animation.js
+++ modules/javascript/projects/balloonsburst/js/animation.js
@@ -10,18 +10,48 @@ function loader(){
 
 // Abre o jogo
 function gameContainer(){
+
   document.getElementById('game-level').style.animation = 'level-fadeout 700ms cubic-bezier(.45,-0.58,.67,.53) forwards';
 
   load = setTimeout(function(){
+    getLevel();
+    setBalloons();
     document.getElementById('game-level').style.visibility = 'hidden';
     document.getElementById('game-container').style.visibility = 'visible';
     document.getElementById('game-container').style.animation = 'game-load 1000ms ease-in-out forwards';
     var gameStart = setTimeout(function(){
       document.getElementById('game').style.visibility = 'visible';
       document.getElementById('game').style.animation = 'game-fadein 1000ms ease-in-out forwards';
-      startGame();
+      counter()
       clearTimeout(gameStart);
     },1200);
     clearTimeout(load);
   }, 700)
+}
+
+// Função do contador
+function counter(){
+  var counting = 3;
+
+  var counter = setInterval(function(){
+
+    var htmlCounter = document.getElementsByClassName('counter');
+
+    if(counting == 0){
+      htmlCounter[0].innerHTML = "GO";
+      htmlCounter[1].innerHTML = "GO";
+    }else{
+      htmlCounter[0].innerHTML = counting;
+      htmlCounter[1].innerHTML = counting;
+    }
+
+    if( counting == -1){
+      startGame();
+      clearInterval(counter);
+      document.getElementById('count').style.visibility = 'hidden';
+    }
+
+    counting = counting - 1;
+  },1000);
+
 }
--- modules/javascript/projects/balloonsburst/js/script.js
+++ modules/javascript/projects/balloonsburst/js/script.js
@@ -4,6 +4,14 @@ var timer = null;
 
 // Função para iniciar o jogo
 function startGame(){
+
+
+  // Inserir o valor do timer
+  timerTick(5) // Função que faz o timer funcionar
+}
+
+// Função para verificar o nivel
+function getLevel(){
   // Captura o nivel do jogo
   game_level = document.getElementById('level-select').value;
 
@@ -20,22 +28,34 @@ function startGame(){
       break;
   }
 
-  // Inserir o valor do timer
-  //document.getElementById('header').innerHTML = game_level;
-  timerTick(5) // Função que faz o timer funcionar
+  document.getElementById('timer-tick').innerHTML = timer;
+
 }
 
 // Função para contar o tempo.
 function timerTick(tim){
   var time = null;
+  tim -= 1;
   timer = setInterval(function(){
-    document.getElementById('timer-tick').innerHTML = tim;
-    if( tim == 0){
+    if( tim == -1){
       clearInterval(timer);
       alert('PERDEU PLAYBOY');
     }else{
+      document.getElementById('timer-tick').innerHTML = tim;
       tim = tim - 1;
     }
   },1000);
 
+}
+
+
+// Função para gerar os balõe
+function setBalloons(){
+  for(var i = 1; i <= 10; i++){
+    var baloes = document.createElement('img');
+    baloes.src = 'imgs/balao_azul_grande.png';
+    baloes.style.margin = '11px';
+    baloes.setAttribute('id', 'b'+i);
+    document.getElementById('game-inner').appendChild(baloes);
+  }
 }
