14996fa09bb862f82be5610621402dccdd3ab2b3
Daniel Costa Munhoz <dc.munhoz@hotmail.com>
2018-04-19 23:42:11 -0300

    Edições no inicio do jogo

--- modules/javascript/projects/balloonsburst/js/animation.js
+++ modules/javascript/projects/balloonsburst/js/animation.js
@@ -16,10 +16,12 @@ function gameContainer(){
     document.getElementById('game-level').style.visibility = 'hidden';
     document.getElementById('game-container').style.visibility = 'visible';
     document.getElementById('game-container').style.animation = 'game-load 1000ms ease-in-out forwards';
-    clearTimeout(load);
     var gameStart = setTimeout(function(){
+      document.getElementById('game').style.visibility = 'visible';
+      document.getElementById('game').style.animation = 'game-fadein 1000ms ease-in-out forwards';
       startGame();
       clearTimeout(gameStart);
     },1200);
+    clearTimeout(load);
   }, 700)
 }
--- modules/javascript/projects/balloonsburst/js/script.js
+++ modules/javascript/projects/balloonsburst/js/script.js
@@ -21,21 +21,19 @@ function startGame(){
   }
 
   // Inserir o valor do timer
-  document.getElementById('timer-tick').innerHTML = timer;
-  timerTick(timer) // Função que faz o timer funcionar
-
-  document.getElementById('header').innerHTML = game_level;
+  //document.getElementById('header').innerHTML = game_level;
+  timerTick(5) // Função que faz o timer funcionar
 }
 
 // Função para contar o tempo.
 function timerTick(tim){
   var time = null;
   timer = setInterval(function(){
-    if( tim == -1){
-      alert('PERDEU PLAYBOY');
+    document.getElementById('timer-tick').innerHTML = tim;
+    if( tim == 0){
       clearInterval(timer);
+      alert('PERDEU PLAYBOY');
     }else{
-      document.getElementById('timer-tick').innerHTML = tim;
       tim = tim - 1;
     }
   },1000);
