390e48e6fe07fc22f1f5a246a2187be83ccd1276
Daniel Costa Munhoz <dc.munhoz@hotmail.com>
2018-05-01 20:48:41 -0300

    Aula PHP Laço de repetição

--- modules/php/12-repeats/index.php
+++ modules/php/12-repeats/index.php
@@ -14,7 +14,44 @@
     echo "Repetição ". $j . "<br>";
     $j++;
   }
+  echo "<br>";
 
+  // Laço Do While
+  echo "Laço Do While <br><br>";
+  $k = 1;
+  do{
+    echo "Repetição " . $k . "<br>";
+    $k++;
+  }while($k <= 10);
+
+  echo "<br>";
+
+  echo "Percorrer uma matriz com laços de repetição<br><br>";
+  $matriz = [];
+  for ($d = 0; $d < 10; $d++){
+    for ($e = 0; $e < 10; $e++){
+      if ($d == $e){
+        $matriz[$d][$e] = 1;
+      }else{
+        $matriz[$d][$e] = 0;
+      }
+      echo $matriz[$d][$e];
+    }
+    echo "<br>";
+  }
+
+  echo "<br>";
+
+  echo "Percorrer uma matriz com foreach<br><br>";
+  $produto[0] = "Mesa";
+  $produto[1] = "Sofá";
+  $produto[2] = "Cama";
+  $produto[3] = "Geladeira";
+  $produto[4] = "TV";
+
+  foreach ($produto as $prod){
+    echo $prod . "<br>";
+  }
 
 
  ?>
