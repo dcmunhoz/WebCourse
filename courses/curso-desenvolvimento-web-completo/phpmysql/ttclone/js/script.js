40499f96bffba794bd2f6fadaf424c707287ac39
Daniel Costa Munhoz <dc.munhoz@hotmail.com>
2018-07-19 21:33:19 -0300

    Change courses folder path

---
+++ courses/curso-desenvolvimento-web-completo/phpmysql/ttclone/js/script.js
@@ -0,0 +1,67 @@
+// Acionar div Login
+var btnEntrar = document.getElementById('menuBtnEntrar');
+var active = 0;
+
+btnEntrar.onclick = function(){
+
+  if( active === 0){
+    document.getElementById('login-box').style.display = 'inline-block';
+    active = 1;
+  }else{
+    document.getElementById('login-box').style.display = 'none';
+    active = 0;
+  }
+};
+
+
+// Verifica se o usuário e senha esta correto.
+var error = 0;
+window.onload = function(){
+    verificaParametros();
+    if(error == 1){
+        document.getElementById('login-box').style.display = 'inline-block';
+        document.getElementById('login-error').style.display = 'inline-block';
+        active = 1;
+    }else{
+        document.getElementById('login-box').style.display = 'none';
+        document.getElementById('login-error').style.display = 'none';
+        active = 0;
+    }
+};
+
+function verificaParametros(){
+    try{
+        var url = window.location;
+        var params = url.toString().split("?")[1];
+        error = params.split("=")[1];
+        
+    }catch (e){
+    }
+}
+
+// Verifica se os campos de usuário e senha estão preenchidos
+document.getElementById("btnLogin").onclick = function(){
+    var usuarioLoginCampo = document.getElementById("user-login");
+    var senhaLoginCampo   = document.getElementById("pwd-login");
+    var usuarioLogin      = usuarioLoginCampo.value;
+    var senhaLogin        = senhaLoginCampo.value;
+    var campoVazio        = false;
+    
+    if(usuarioLogin == ''){
+        usuarioLoginCampo.classList.add('input-error');
+        campoVazio = true;
+    }else{
+        usuarioLoginCampo.classList.remove('input-error');
+    }
+    
+    if(senhaLogin == ''){
+        senhaLoginCampo.classList.add('input-error');
+        campoVazio = true;
+    }else{
+        senhaLoginCampo.classList.remove('input-error');
+        
+    }
+    
+    if (campoVazio) return false;
+    
+};
