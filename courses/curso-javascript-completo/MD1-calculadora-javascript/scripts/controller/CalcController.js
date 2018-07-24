8978ec10c86e1fc1de02a1eeae76715cff54eca6
Daniel Munhoz <dc.munhoz@hotmail.com>
2018-07-24 12:38:49 -0300

    JS - C10 - Adicionando operações - isNaN e Array.lenght

--- courses/curso-javascript-completo/MD1-calculadora-javascript/scripts/controller/CalcController.js
+++ courses/curso-javascript-completo/MD1-calculadora-javascript/scripts/controller/CalcController.js
@@ -48,9 +48,43 @@ class CalcController{
 
     }
 
+    getLastOperation(){
+
+        return this._operation[this._operation.length - 1];
+
+    }
+
+    setLastOperation(value){
+
+        this._operation[this._operation.length-1] = value;
+
+    }
+
+    isOperator(value){
+
+        return (['+', '-', '*', '%', '/'].indexOf(value) > -1);
+
+    }
+
     addOperation(value){
 
-        this._operation.push(value);
+        if(isNaN(this.getLastOperation())){
+
+            if(this.isOperator(value)){
+                
+                this.setLastOperation(value);
+
+            }else if(isNaN(value)){
+                // Outra coisa
+                console.log(value);
+            }else{    
+                this._operation.push(value);
+            }
+
+        }else{
+            let newValue = this.getLastOperation().toString() + value.toString();
+            this.setLastOperation(parseInt(newValue));
+        }
         console.log(this._operation);
     }
 
@@ -70,24 +104,26 @@ class CalcController{
                 this.clearEntry();
                 break;
             case 'soma':
-                
+                this.addOperation('+');
                 break;
             case 'subtracao':
-                
+                this.addOperation('-');
                 break;
             case 'multiplicacao':
-                
+                this.addOperation('*');
                 break;
             case 'divisao':
-                
+                this.addOperation('/');
                 break;
             case 'porcentagem':
-                
+                this.addOperation('%');
                 break;
             case 'igual':
-                
+            
                 break;
-
+            case 'ponto':
+                this.addOperation('.');
+                break;
             case '0':
             case '1':
             case '2':
