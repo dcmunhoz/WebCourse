3abb0ccb87b8e786af9ce6fefbf2c0287c7acd6c
Daniel Munhoz <dc.munhoz@hotmail.com>
2018-07-24 08:58:58 -0300

    C05 - Data e Hora

--- courses/curso-javascript-completo/MD1-calculadora-javascript/scripts/controller/CalcController.js
+++ courses/curso-javascript-completo/MD1-calculadora-javascript/scripts/controller/CalcController.js
@@ -2,40 +2,67 @@ class CalcController{
     
     // Class constructor
     constructor(){
-        
-        this._displayCalc = "0";
+        this._locale          = 'pt-BR';
+        this._displayCalcEl   = document.querySelector("#display");
+        this._dateEl          = document.querySelector("#data");
+        this._timeEl          = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
         
     }
-    
+
+    // ===== Functions =====    
     initialize(){
+        
+        // Updates date and time each second
+        this.setDisplayDateTime();
+        setInterval(()=>{
+            this.setDisplayDateTime();
+        }, 1000);
 
-        let displayCalcEl   = document.querySelector("#display");
-        let dateEl          = document.querySelector("#data");
-        let timeEl          = document.querySelector("#hora");
-
-        displayCalcEl.innerHTML = "4567";
-        dateEl.innerHTML  = "01/01/2019";   
-        timeEl.innerHTML  = "00:00";
 
     }
 
-    // Getters & Setters
+    setDisplayDateTime(){
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale,{day: '2-digit', month: 'long', year: 'numeric'});
+    }
+
+    // ===== Getters & Setters =====
+    // Display calculator
     get displayCalc(){
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
     }
 
-    set displayCalc(valor){
-        this._displayCalc = valor;
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value;
     }
 
-    get dataAtual(){
-        return this._currentDate;
+    // Time calculator
+    get displayTime(){
+        return this._timeEl.innerHTML;
     }
 
-    set dataAtual(valor){
-        this._currentDate = valor;
+    set displayTime(value){
+        this._timeEl.innerHTML = value;
+    }
+
+    // Date calculator
+    get displayDate(){
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value){
+        this._dateEl.innerHTML = value;
+    }
+
+    // Return date instance
+    get currentDate(){
+        return new Date();
+    }
+
+    set currentDate(value){
+        this._currentDate = value;
     }
 
 }
