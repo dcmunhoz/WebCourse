cf39e6ae46a72a1f05ebe62ac6bde21b81142931
Daniel Munhoz <dc.munhoz@hotmail.com>
2018-07-30 16:11:39 -0300

    PHP - Utilizando Variáveis com PHP

---
+++ courses/curso-php-completo/md03-conhecendo-linguagem/1-variaveis/index.php
@@ -0,0 +1,12 @@
+<?php
+
+// Para iniciar uma variavel sempre colocamos '$'(Cifão/Dollar) seguida do nome
+$nome = "Daniel";
+
+//Exibir o dado de uma variavel na tela
+echo($nome);
+
+echo("<br>");
+
+//Exibir o tipo de dado contido nessa variável
+var_dump($nome);
