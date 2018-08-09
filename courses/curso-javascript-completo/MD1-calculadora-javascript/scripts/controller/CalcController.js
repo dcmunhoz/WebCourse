7e70fd5bc622b3ad34cf1933ed1dae0ba0ca4546
Daniel Costa Munhoz <dc.munhoz@hotmail.com>
2018-08-08 22:48:16 -0300

    JS - C19 - Trabalhando com Áudio

--- courses/curso-javascript-completo/MD1-calculadora-javascript/scripts/controller/CalcController.js
+++ courses/curso-javascript-completo/MD1-calculadora-javascript/scripts/controller/CalcController.js
@@ -3,7 +3,8 @@ class CalcController{
     // Class constructor
     constructor(){
 
-
+        this._audio           = new Audio("click.mp3");
+        this._audioOnOff      = false;
         this._lastOperator    = "";
         this._lastNumber      = "";
         this._operation       = [];
@@ -52,11 +53,36 @@ class CalcController{
 
         this.setLastNumberToDisplay();
         this.pasteFromClipboard();
+
+        document.querySelectorAll(".btn-ac").forEach(btn=>{
+            btn.addEventListener("dblclick", e=>{
+                this.toggleAudio();
+            });
+        });
+    }
+
+    toggleAudio(){
+        
+        this._audioOnOff = !this._audioOnOff;
+
+    }
+
+    playAudio(){
+
+        if(this._audioOnOff){
+
+            this._audio.currentTime = 0;
+            this._audio.play();
+        }
+
     }
 
     initKeyboard(){
 
         document.addEventListener('keyup', e=>{
+
+            
+            this.playAudio();
 
             switch(e.key){
                 case 'Escape':
@@ -305,6 +331,8 @@ class CalcController{
 
     execBtn(value){
 
+        this.playAudio();
+
         switch(value){
             case 'ac':
                 this.clearAll();
@@ -349,6 +377,8 @@ class CalcController{
                 this.setError();
                 break;
         }
+
+
 
     }
 
