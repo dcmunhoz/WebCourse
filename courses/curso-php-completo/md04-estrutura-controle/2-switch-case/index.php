fa77255aa19de8b7ea21292e1e362735ff59b0c6
Daniel Munhoz <dc.munhoz@hotmail.com>
2018-08-09 17:41:02 -0300

    PHP - MD04 - 21. Switch Case

---
+++ courses/curso-php-completo/md04-estrutura-controle/2-switch-case/index.php
@@ -0,0 +1,32 @@
+<?php
+$diaSemana = 8; //date("w");
+
+switch($diaSemana){
+    case 0:
+        echo "Domingo";
+        break;
+    case 1:
+        echo "Segunda";
+        break;
+    case 2:
+        echo "Terça";
+        break;
+    case 3:
+        echo "Quarta";
+        break;
+    case 4:
+        echo "Quinta";
+        break;
+    case 5:
+        echo "Sexta";
+        break;
+    case 6:
+        echo "Sabado";
+        break;
+    default:
+        echo "Data Invalida";
+        break;
+}
+
+
+?>
