72f43ec4f22635c87f93e4f749fed5d86cbe46b2
Daniel Munhoz <dc.munhoz@hotmail.com>
2018-08-13 10:29:37 -0300

    JS - G4 - Variáveis e Seletores

--- courses/curso-javascript-completo/MD2-gerenciamento-usuario/index.js
+++ courses/curso-javascript-completo/MD2-gerenciamento-usuario/index.js
@@ -1,4 +1,8 @@
-var nome = document.querySelector("#exampleInputName");
-
-nome.value = "Daniel";
-nome.style.color = "red";
+var nameEl          = document.querySelector("#exampleInputName");
+var genderEl        = document.querySelectorAll("#form-user-create [name=gender]:checked");
+var birthdayEl      = document.querySelector("#exampleInputBirth");
+var countryEl       = document.querySelector("#exampleInputCountry");
+var emailEl         = document.querySelector("#exampleInputEmail");
+var passEl          = document.querySelector("#exampleInputPassword");
+var photoEl         = document.querySelector("#exampleInputFile");
+var adminEl         = document.querySelector("#exampleInputAdmin");
