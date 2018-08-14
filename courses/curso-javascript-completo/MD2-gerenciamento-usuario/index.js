912bca9da108fcdeaca6c065015f1d3e7a123033
Daniel Munhoz <dc.munhoz@hotmail.com>
2018-08-14 11:05:58 -0300

    JS - G8 - Trabalhando com Eventos no JavaScript

--- courses/curso-javascript-completo/MD2-gerenciamento-usuario/index.js
+++ courses/curso-javascript-completo/MD2-gerenciamento-usuario/index.js
@@ -2,22 +2,26 @@
 var fields  = document.querySelectorAll("#form-user-create [name]");
 var user    = {};     
 
-fields.forEach((field, index)=>{
+document.getElementById("form-user-create").addEventListener("submit", function(event){
+    event.preventDefault();
+
+    fields.forEach((field, index)=>{
    
-    if(field.name == 'gender'){
-
-        if(field.checked){
-
-            user[field.name] = field.value;
-
-        }
-
-    }else{
-
-        user[field.name] = field.value;
+        if(field.name == 'gender'){
     
-    }
+            if(field.checked){
+    
+                user[field.name] = field.value;
+    
+            }
+    
+        }else{
+    
+            user[field.name] = field.value;
+        
+        }
+    
+    });
 
+    console.log(user);
 });
-
-console.log(user);
