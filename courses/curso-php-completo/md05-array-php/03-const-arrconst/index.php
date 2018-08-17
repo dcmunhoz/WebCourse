a6be579ddeeb63e96dda020e62f0948ebcd92d1c
Daniel Munhoz <dc.munhoz@hotmail.com>
2018-08-17 09:22:44 -0300

    PHP - MD05 - Constantes e Array Constantes

---
+++ courses/curso-php-completo/md05-array-php/03-const-arrconst/index.php
@@ -0,0 +1,27 @@
+<?php
+
+    define("SERVIDOR", '127.0.0.1');
+    echo SERVIDOR;
+    echo "<br>";
+    echo "<br>";
+
+    define("BANCO_DE_DADOS", [
+        '127.0.0.1',
+        'root',
+        'pwwd',
+        'teste'
+    ]);
+
+    print_r(BANCO_DE_DADOS);
+    echo "<br>";
+    echo "<br>";
+
+
+    echo PHP_VERSION;
+    echo "<br>";
+    echo DIRECTORY_SEPARATOR;
+    
+
+
+
+?>
