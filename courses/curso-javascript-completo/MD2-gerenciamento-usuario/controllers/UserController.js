6422a5984df79b4174b203c0c3ef3eee20480b6b
Daniel Munhoz <dc.munhoz@hotmail.com>
2018-08-28 09:07:27 -0300

    Js - G28 - Armazenando Dados com localStorage

--- courses/curso-javascript-completo/MD2-gerenciamento-usuario/controllers/UserController.js
+++ courses/curso-javascript-completo/MD2-gerenciamento-usuario/controllers/UserController.js
@@ -203,9 +203,9 @@ class UserController{
 
         let users = [];
         
-        if(sessionStorage.getItem("users")){
+        if(localStorage.getItem("users")){
 
-            users = JSON.parse(sessionStorage.getItem("users"));
+            users = JSON.parse(localStorage.getItem("users"));
 
         }
 
@@ -235,7 +235,8 @@ class UserController{
 
         user.push(data);
 
-        sessionStorage.setItem("users", JSON.stringify(user));
+        //sessionStorage.setItem("users", JSON.stringify(user));
+        localStorage.setItem("users", JSON.stringify(user));
 
     }
 
