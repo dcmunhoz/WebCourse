d4229a20d4a02eb955ae55504ba6441d8096ceef
Daniel Munhoz <dc.munhoz@hotmail.com>
2018-08-30 16:40:32 -0300

    JS - N06 - Nodemon e Criando o Servidor com Express

--- courses/curso-javascript-completo/MD3-nodejs/02-node/index.js
+++ courses/curso-javascript-completo/MD3-nodejs/02-node/index.js
@@ -1,31 +1,25 @@
-const http = require('http');
+const express = require('express');
 
-let server = http.createServer((req, res)=>{
+let app = express();
 
-    console.log("URL: ", req.url);
-    console.log("METHOD: ", req.method);
+app.get('/', (req, res)=>{
 
-    switch(req.url){
-        case '/':
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-            res.end('<h1> Olá </h1>');
-        break;
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1> Olá </h1>');
+});
 
-        case '/users':
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify({
-                users:[{
-                    name:'hcode',
-                    email:'hcode@teste.com'
-                }]
-            }));
-        break;
-    }
+app.get('/users', (req, res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
+        users:[{
+            name:'hcode',
+            email:'hcode@teste.com'
+        }]
+    });
+});
 
-})
-
-server.listen(3000, '127.0.0.1', ()=>{
+app.listen(3000, '127.0.0.1', ()=>{
     console.log("Server OK!");
 });
