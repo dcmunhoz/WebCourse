1c9ad0072a52317b858ab50eb84295c1fbcd0db8
Daniel Munhoz <dc.munhoz@hotmail.com>
2018-09-03 13:17:16 -0300

    JS - N11 - Listando os usuários do banco NeDB

--- courses/curso-javascript-completo/MD3-nodejs/02-node/routes/users.js
+++ courses/curso-javascript-completo/MD3-nodejs/02-node/routes/users.js
@@ -6,16 +6,27 @@ let db = new nedb({
 
 module.exports = app=>{
 
+    
     app.get('/users', (req, res)=>{
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({
-            users:[{
-                name:'hcode',
-                email:'hcode@teste.com',
-                id:1
-            }]
-        });
+
+        db.find({}).sort({name:1}).exec((err, users)=>{
+            if(err){
+                console.log('Erro: ${err} ');
+                res.status(400).json({
+                    error: err
+                });
+            }else{
+
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json({
+                    users
+                });
+            }
+
+        })
+
+        
     });
     
     app.post('/users', (req, res)=>{
