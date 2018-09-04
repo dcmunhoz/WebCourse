01387360cd590f9677b709763cb8c7d1d931a614
Daniel Munhoz <dc.munhoz@hotmail.com>
2018-09-04 16:51:01 -0300

    JS - N16 - Validando dados recebidos via Post

--- courses/curso-javascript-completo/MD3-nodejs/02-node/index.js
+++ courses/curso-javascript-completo/MD3-nodejs/02-node/index.js
@@ -1,11 +1,13 @@
 const express       = require('express');
 const consign       = require('consign');
-const bodyParser   = require('body-parser');
+const bodyParser    = require('body-parser');
+const expressValid  = require('express-validator');
 
 let app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(expressValid());
 consign().include('routes').include('utils').into(app);
 
 
