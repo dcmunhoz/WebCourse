9e0989bb5e918d43c723e233e5efb2e1fbbbc6ca
Daniel Munhoz <dc.munhoz@hotmail.com>
2018-09-06 11:45:29 -0300

    PHP - MD09 - Criando uma Classe

---
+++ courses/curso-php-completo/md09-poo/01-criando/index.php
@@ -0,0 +1,22 @@
+<?php
+
+class Pessoa{
+
+    public $nome;
+
+    public function falar(){
+
+        return "O meu nome é " . $this->nome;
+
+    }
+
+}
+
+#-----------------------------------------------------
+
+$pessoa = new Pessoa();
+
+$pessoa->nome = 'Daniel';
+echo $pessoa->falar();
+
+?>
