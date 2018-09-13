7e33183d649c1195c10330e5944eeb546e8144a6
Daniel Munhoz <dc.munhoz@hotmail.com>
2018-09-13 13:00:02 -0300

    PHP - MD06 - Herança

--- courses/curso-php-completo/md09-poo/06-heranca/index.php
+++ courses/curso-php-completo/md09-poo/06-heranca/index.php
@@ -1,4 +1,41 @@
 <?php
 
+    class Documento{
+
+        private $numero;
+
+        public function getNumero(){
+            return $this->numero;
+        }
+
+        public function setNumero($n){
+            $this->numero = $n;
+        }
+
+
+    }
+
+    class CPF extends Documento{
+        public function validar():bool{
+            
+            $numeroCPF = $this->getNumero();
+
+            // Aqui vai a validação do CPF
+
+            return true;
+        }
+
+    }
+
+    #--------------------------------------
+
+    $doc = new CPF();
+
+    $doc->setNumero("123456789-11");
+
+    var_dump($doc->validar());
+    echo "<br>";
+    echo $doc->getNumero();
+
 
 ?>
