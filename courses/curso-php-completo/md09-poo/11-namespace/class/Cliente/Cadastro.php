d1482056af3564ea59620d3e05f19d33822b4b2b
Daniel Munhoz <dc.munhoz@hotmail.com>
2018-09-14 17:41:26 -0300

    PHP - MD09 - Namespace

---
+++ courses/curso-php-completo/md09-poo/11-namespace/class/Cliente/Cadastro.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Cliente;
+
+class Cadastro extends \Cadastro{
+
+    public function registrarVenda(){
+
+        echo "Venda registrada para o cliente " . $this->getNome();
+
+
+    }
+
+
+}
+
+?>
