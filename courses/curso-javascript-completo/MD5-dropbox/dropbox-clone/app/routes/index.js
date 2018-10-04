0df13c58f306877588ba1af33fa22fe4af8c737d
Daniel Munhoz <dc.munhoz@hotmail.com>
2018-10-04 13:05:56 -0300

    JS - D19 - Criando rota para abri arquivo

--- courses/curso-javascript-completo/MD5-dropbox/dropbox-clone/app/routes/index.js
+++ courses/curso-javascript-completo/MD5-dropbox/dropbox-clone/app/routes/index.js
@@ -8,6 +8,40 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/file', (req, res)=>{
+
+  let path = './' + req.query.path;
+
+  if (fs.existsSync(path)){
+
+    fs.readFile(path, (err, data)=>{
+
+      if(err){
+
+        console.error(err);
+        res.status(400).json({
+          error: err
+        });
+
+      }else{
+
+        res.status(200).end(data)
+
+      }
+
+    });
+
+  }else{
+
+    res.status(400).json({
+      error:'file not found'
+    });
+
+  }
+
+
+});
+
 router.delete('/file', (req, res)=>{
   
   let form = new formidable.IncomingForm({
@@ -34,8 +68,14 @@ router.delete('/file', (req, res)=>{
 
       });
 
+    }else{
+
+      res.status(400).json({
+        error:'file not found'
+      });
+
     }
-    
+
   });
 
 });
