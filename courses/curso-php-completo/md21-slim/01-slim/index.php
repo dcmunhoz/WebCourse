ea7389e873ce7e2d00258770ce654e04fdaedaf4
Daniel Munhoz <dc.munhoz@hotmail.com>
2018-10-30 17:14:41 -0300

    PHP - MD21 - Slim Framework 2.0

---
+++ courses/curso-php-completo/md21-slim/01-slim/index.php
@@ -0,0 +1,22 @@
+<?php
+
+    require_once("vendor/autoload.php");
+
+    $app = new \Slim\Slim();
+
+    $app->get('/', function(){
+        echo json_encode(array(
+            "name"=>"Daniel",
+            "data"=>date("Y-m-d H:i:s")
+
+        ));
+    });
+
+    $app->get('/hello/:name', function($name){
+        echo "Hello, " . $name;
+
+    });
+
+    $app->run();
+
+?>
