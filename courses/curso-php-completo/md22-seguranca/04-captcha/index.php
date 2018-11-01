73f55a193f6c28895b45e49305f88835d53b1fe8
Daniel Munhoz <dc.munhoz@hotmail.com>
2018-11-01 17:40:29 -0300

    PHP - MD22 - Usando o reCaptch para impedir robos

---
+++ courses/curso-php-completo/md22-seguranca/04-captcha/index.php
@@ -0,0 +1,8 @@
+<script src='https://www.google.com/recaptcha/api.js'></script>
+<form method="POST" action="cadastrar.php">
+    <input type="email" name="email">
+    <button type="submit">Enviar</button>
+    <br>
+    <div class="g-recaptcha" data-sitekey="6Le2OngUAAAAAIg6Q-yk0llorjZFBCuODfGqCihe"></div>
+
+</form>
