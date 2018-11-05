5a1dffcf5f888a42b426975f9e8368edf7dc54bc
Daniel Munhoz <dc.munhoz@hotmail.com>
2018-11-05 16:46:09 -0200

    PHP - MD22 - XSS

---
+++ courses/curso-php-completo/md22-seguranca/05-xss/index.php
@@ -0,0 +1,17 @@
+<form method="POST" action="">
+    <input type="text" name="busca">
+    <button type="submit">Enviar</button>
+
+</form>
+
+<?php
+
+
+
+    if(isset($_POST['busca'])){
+        echo strip_tags($_POST['busca']);
+        echo "<br>";
+        echo htmlentities($_POST['busca']);
+    }
+
+?>
