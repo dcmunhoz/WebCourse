a911f3a3e868923a5e3886ec6c8fcc6f05d79e1a
Daniel Munhoz <dc.munhoz@hotmail.com>
2018-11-08 17:12:23 -0200

    PHP - MD23 - Autoload das classes do projeto

--- courses/curso-php-completo/md23-project/vendor/composer/autoload_static.php
+++ courses/curso-php-completo/md23-project/vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitae876b4276ce9cff389dd5dcbbc67759
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInitae876b4276ce9cff389dd5dcbbc67759
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInitae876b4276ce9cff389dd5dcbbc67759::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitae876b4276ce9cff389dd5dcbbc67759::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitae876b4276ce9cff389dd5dcbbc67759::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInitae876b4276ce9cff389dd5dcbbc67759::$prefixesPsr0;
             $loader->classMap = ComposerStaticInitae876b4276ce9cff389dd5dcbbc67759::$classMap;
 
