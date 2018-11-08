556132db6c983ef6b750d920de8dde0e03088c7a
Daniel Munhoz <dc.munhoz@hotmail.com>
2018-11-08 16:59:59 -0200

    PHP - MD23 - Configurando Projeto

--- courses/curso-php-completo/md23-project/index.php
+++ courses/curso-php-completo/md23-project/index.php
@@ -1,5 +1,17 @@
-<?php
+<?php 
 
-    echo "E-Commerce";
+require_once("vendor/autoload.php");
 
-?>
+$app = new \Slim\Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function() {
+    
+	echo "OK";
+
+});
+
+$app->run();
+
+ ?>
