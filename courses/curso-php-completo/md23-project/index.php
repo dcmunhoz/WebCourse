1d0dc81bcc68526d591edf594d282fb583e6be4f
Daniel Munhoz <dc.munhoz@hotmail.com>
2018-11-20 17:15:40 -0200

    PHP - MD23 - Admin, esqueceu senha /p1

--- courses/curso-php-completo/md23-project/index.php
+++ courses/curso-php-completo/md23-project/index.php
@@ -152,7 +152,22 @@ $app->post('/admin/users/:iduser', function($iduser){
 	exit;
 });
 
+$app->get('/admin/forgot/', function(){
 
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot");
+
+});
+
+$app->post('/admin/forgot/', function(){
+
+	$user = User::getForgot($_POST['email']);
+
+});
 
 $app->run();
 
--- courses/curso-php-completo/md23-project/vendor/hcodebr/php-classes/src/Model/User.php
+++ courses/curso-php-completo/md23-project/vendor/hcodebr/php-classes/src/Model/User.php
@@ -136,6 +136,45 @@
             
         }
 
+        public static function getForgot($email){
+
+            $sql = new Sql();
+
+            $result = $sql->select("SELECT * FROM tb_persons a JOIN tb_users b USING (idperson) WHERE a.desemail = :email ;", array(
+                ":email"=>$email
+            ));
+
+            if (count($result[0]) === 0){
+
+                throw new \Exception("Não foi possivel recuperar a senha.");
+                
+            }else{
+
+                $data = $result[0];
+
+                $result2 = $sql->select("CALL sp_userspasswordsrecoveries_create(:idusuario, :desip)", array(
+                    ":iduser"=>$data["iduser"],
+                    ":desip"=>$_SERVER["REMOTE_ADDR"]
+                ));
+
+                if( count($result2) === 0 ){
+
+                    throw new \Exception("Não foi possivel recuperar a senha.");
+
+                }else{
+
+                    $dataRecovery = $result2[0];
+
+                    // Pausa - 14:08
+                    base64_encode();
+
+
+                }
+
+            }
+
+        }
+
 
     }
 
