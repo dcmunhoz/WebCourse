169a6470d25d50ae5a70cfe249724e4362b05a5a
Daniel Munhoz <dc.munhoz@hotmail.com>
2018-12-14 11:56:56 -0200

    PHP - MD23 - Site Categorias

--- courses/curso-php-completo/md23-project/vendor/hcodebr/php-classes/src/Model/Category.php
+++ courses/curso-php-completo/md23-project/vendor/hcodebr/php-classes/src/Model/Category.php
@@ -27,6 +27,8 @@
 
             $this->setData($result[0]);
 
+            Category::updateFile();
+
         }
 
         public function get($idCategory){
@@ -48,6 +50,25 @@
             $sql->query("DELETE FROM tb_categories WHERE idcategory = :idcategory",[
                 ':idcategory'=>$this->getidcategory()
             ]);
+
+            Category::updateFile();
+
+        }
+
+        public static function updateFile(){
+
+            $categories = Category::listAll();
+
+            $html = [];
+
+            foreach($categories as $row){
+
+                array_push($html, '<li><a href="/WebCourse/courses/curso-php-completo/md23-project/index.php/category/'.$row['idcategory'].'">'. $row['descategory'] .'</a></li>');
+
+            }
+
+            file_put_contents($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "\WebCourse\courses\curso-php-completo\md23-project" . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . "categories-menu.html", implode('', $html));
+
 
         }
 
