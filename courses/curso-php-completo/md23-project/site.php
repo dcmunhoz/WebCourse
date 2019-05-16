94067f7a7704caaf7b7e5f6d259bb9fc9c96ed04
Daniel Munhoz <dc.munhoz@hotmail.com>
2019-05-15 22:25:24 -0300

    PHP - MD24 - Site alterar senha

--- courses/curso-php-completo/md23-project/site.php
+++ courses/curso-php-completo/md23-project/site.php
@@ -506,7 +506,7 @@ $app->get("/order/:idorder", function($idorder){
 
 $app->get('/boleto/:idorder', function($idorder){
 
-	User::verifyLogin();
+	User::verifyLogin(false);
 	$order = new Order();
 	$order->get((int)$idorder);
 
@@ -575,7 +575,7 @@ $app->get('/boleto/:idorder', function($idorder){
 
 $app->get("/profile/orders", function(){
 
-	User::verifyLogin();
+	User::verifyLogin(false);
 
 	$user = User::getFromSession();
 
@@ -588,7 +588,7 @@ $app->get("/profile/orders", function(){
 
 $app->get("/profile/orders/:idorder", function($idorder){
 
-	User::verifyLogin();
+	User::verifyLogin(false);
 
 	$order = new Order();
 	$cart = new Cart();
@@ -606,6 +606,71 @@ $app->get("/profile/orders/:idorder", function($idorder){
 		'cart'=>$cart->getValues(),
 		'products'=>$cart->getProducts()
 	]);
+
+});
+
+$app->get("/profile/change-password", function(){
+
+	User::verifyLogin(false);
+
+	$page = new Page();
+
+	$page->setTpl("profile-change-password",[
+		'changePassError'=>User::getError(),
+		'changePassSuccess'=>User::getSuccess()
+	]);
+
+
+});
+
+$app->post("/profile/change-password", function(){
+
+	User::verifyLogin(false);
+
+	if (!isset($_POST['current_pass']) || $_POST['current_pass'] === "") {
+
+		User::setError("Digite a senha atual. ");
+		header("Location: /WebCourse/courses/curso-php-completo/md23-project/index.php/profile/change-password");
+		exit;
+	}
+
+	if (!isset($_POST['new_pass']) || $_POST['new_pass'] === "") {
+
+		User::setError("Digite a nova senha. ");
+		header("Location: /WebCourse/courses/curso-php-completo/md23-project/index.php/profile/change-password");
+		exit;
+	}
+
+	if (!isset($_POST['new_pass_confirm']) || $_POST['new_pass_confirm'] === "") {
+
+		User::setError("Confirme a nova senha ");
+		header("Location: /WebCourse/courses/curso-php-completo/md23-project/index.php/profile/change-password");
+		exit;
+	}
+
+	if ($_POST['current_pass'] === $_POST['new_pass'] ) {
+		User::setError("Sua nova senha deve ser diferente da atual. ");
+		header("Location: /WebCourse/courses/curso-php-completo/md23-project/index.php/profile/change-password");
+		exit;
+	}
+
+	$user = User::getFromSession();
+
+	if (!password_verify($_POST['current_pass'], $user->getdespassword())) {
+
+		User::setError("A senha esta invalida. ");
+		header("Location: /WebCourse/courses/curso-php-completo/md23-project/index.php/profile/change-password");
+		exit;
+
+	}
+
+	$user->setdespassword($_POST['new_pass']);
+
+	$user->update();
+
+	User::setSuccess("Senha alterada com sucesso.");
+	header("Location: /WebCourse/courses/curso-php-completo/md23-project/index.php/profile/change-password");
+	exit;
 
 });
 
