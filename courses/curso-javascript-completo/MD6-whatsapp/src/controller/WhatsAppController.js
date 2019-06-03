e4f7977330a7a21fa55474d16f899f3931b55980
dcmunhoz <dc.munhoz@hotmail.com>
2019-06-03 10:16:26 -0300

    JS - MD06A2 - Criando controle principal do APP

--- courses/curso-javascript-completo/MD6-whatsapp/src/controller/WhatsAppController.js
+++ courses/curso-javascript-completo/MD6-whatsapp/src/controller/WhatsAppController.js
@@ -4,6 +4,20 @@ class WhatsAppController {
 
         console.log("Controller OK");
 
+        this.loadElements();
+
+    }
+
+    loadElements(){
+        
+        this.el = {};
+
+        document.querySelectorAll('[id]').forEach(element=>{
+
+            this.el[Format.getCamelCase(element.id)] = element;
+
+        });
+
     }
 
 }
