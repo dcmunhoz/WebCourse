4f5e2420b33163b9616b70eeff814d1fded9e92c
dcmunhoz <dc.munhoz@hotmail.com>
2019-06-10 15:06:18 -0300

    JS - MD06A17 - Parando de capturar imagem

--- courses/curso-javascript-completo/MD6-whatsapp/src/controller/CameraController.js
+++ courses/curso-javascript-completo/MD6-whatsapp/src/controller/CameraController.js
@@ -8,11 +8,23 @@ export class CameraController {
             video:true
         }).then(stream=>{
 
+            this._stream = stream;
+
             this._videoEl.src = URL.createObjectURL(stream);
             this._videoEl.play();
 
         }).catch(err=>{
             console.error(err);
+        });
+
+    }
+
+    stop(){
+
+        this._stream.getTracks().forEach(track=>{
+
+            track.stop();
+
         });
 
     }
