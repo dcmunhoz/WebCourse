0ca839508a0e4f25d6e0c9a17ab038eb3f79d5ba
dcmunhoz <dc.munhoz@hotmail.com>
2019-06-11 14:59:11 -0300

    JS - MD06A20 - Lendo capa e gerando imagem de PDF

--- courses/curso-javascript-completo/MD6-whatsapp/src/controller/WhatsAppController.js
+++ courses/curso-javascript-completo/MD6-whatsapp/src/controller/WhatsAppController.js
@@ -260,20 +260,30 @@ export class WhatsAppController {
             this.el.inputDocument.on("change", e=>{
 
                 if (this.el.inputDocument.files.length) {
-                    
+                    this.el.panelDocumentPreview.css({
+                        'height':'1%'
+                    });
                     let file = this.el.inputDocument.files[0];
 
                     this._documentPreviewController = new DocumentPreviewController(file);
                     
                     this._documentPreviewController.getPreviewData().then(data=>{
                         
+                        
                         this.el.imgPanelDocumentPreview.src = data.src;
                         this.el.infoPanelDocumentPreview.innerHTML = data.info;
                         this.el.imagePanelDocumentPreview.show();
                         this.el.filePanelDocumentPreview.hide();
-                       
+                        
+                        this.el.panelDocumentPreview.css({
+                            'height':'calc(100% - 120px)'
+                        });
+
                         
                     }).catch(err=>{
+                        this.el.panelDocumentPreview.css({
+                            'height':'calc(100% - 120px)'
+                        });
                         switch (file.type) {
                             default:
                                 this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-generic';
