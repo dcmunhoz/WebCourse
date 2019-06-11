43d12a2f1eae80c8b007df12f840cded56c49562
dcmunhoz <dc.munhoz@hotmail.com>
2019-06-11 13:11:18 -0300

    JS - MD06A19 - Enviando mensagens de documento

--- courses/curso-javascript-completo/MD6-whatsapp/src/controller/WhatsAppController.js
+++ courses/curso-javascript-completo/MD6-whatsapp/src/controller/WhatsAppController.js
@@ -1,5 +1,6 @@
 import {Format} from './../util/Format';
 import {CameraController} from './CameraController';
+import {DocumentPreviewController} from './DocumentPreviewController';
 
 export class WhatsAppController {
 
@@ -254,6 +255,38 @@ export class WhatsAppController {
             this.el.panelDocumentPreview.css({
                 'height':'calc(100% - 120px)'
             });
+            this.el.inputDocument.click();
+
+            this.el.inputDocument.on("change", e=>{
+
+                if (this.el.inputDocument.files.length) {
+                    
+                    let file = this.el.inputDocument.files[0];
+
+                    this._documentPreviewController = new DocumentPreviewController(file);
+                    
+                    this._documentPreviewController.getPreviewData().then(data=>{
+                        
+                        this.el.imgPanelDocumentPreview.src = data.src;
+                        this.el.infoPanelDocumentPreview.innerHTML = data.info;
+                        this.el.imagePanelDocumentPreview.show();
+                        this.el.filePanelDocumentPreview.hide();
+                       
+                        
+                    }).catch(err=>{
+                        switch (file.type) {
+                            default:
+                                this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-generic';
+                            break;
+                        }
+
+                        this.el.filenamePanelDocumentPreview.innerHTML = file.name;
+                        this.el.imagePanelDocumentPreview.hide();
+                        this.el.filePanelDocumentPreview.show();
+                    });
+                }
+            });
+
         });
 
         this.el.btnClosePanelDocumentPreview.on('click', e=>{
