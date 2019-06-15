141d253272df36619fdf264b1988879ef814e68c
dcmunhoz <dc.munhoz@hotmail.com>
2019-06-15 14:51:00 -0300

    JS - MD06A37 - Adicionando scroll automatico

--- courses/curso-javascript-completo/MD6-whatsapp/src/controller/WhatsAppController.js
+++ courses/curso-javascript-completo/MD6-whatsapp/src/controller/WhatsAppController.js
@@ -180,28 +180,38 @@ export class WhatsAppController {
             display:'flex'
         }); 
 
+        this.el.panelMessagesContainer.innerHTML = '';
         Message.getRef(this._contactActive.chatId).orderBy('timeStamp').onSnapshot(docs =>{
 
-            this.el.panelMessagesContainer.innerHTML = '';
+
+            let scrollTop = this.el.panelMessagesContainer.scrollTop;
+            let scrollTopMax = this.el.panelMessagesContainer.scrollHeight - this.el.panelMessagesContainer.offsetHeight;
+            let autoScroll = (scrollTop >= scrollTopMax) ? true : false ;
 
             docs.forEach(doc=>{
 
                 let data = doc.data();
                 data.id = doc.id;
-             
-                if(!this.el.panelMessagesContainer.querySelector("#"+data.id)){   
+
+               
+                if(!this.el.panelMessagesContainer.querySelector("#_"+data.id)){
                     let message = new Message();
                     message.fromJSON(data);
                     let me = (data.from == this._user.email);
                     let view = message.getViewElement(me);
 
-                    this.el.panelMessagesContainer.appendChild(view);
+                    this.el.panelMessagesContainer.appendChild(view);    
 
                 }
-
-                
-
             });
+
+            if(autoScroll){
+
+                this.el.panelMessagesContainer.scrollTop = (this.el.panelMessagesContainer.scrollHeight - this.el.panelMessagesContainer.offsetHeight);
+
+            }else{
+                this.el.panelMessagesContainer.scrollTop = scrollTop;
+            }
 
         });
 
