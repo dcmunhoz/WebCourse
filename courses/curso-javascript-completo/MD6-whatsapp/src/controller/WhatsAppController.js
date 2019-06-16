8135c0636fbaa62292dab3c60f0fd94519defff0
dcmunhoz <dc.munhoz@hotmail.com>
2019-06-16 16:07:10 -0300

    JS - MD06A46 - Alterando foto de perfil

--- courses/curso-javascript-completo/MD6-whatsapp/src/controller/WhatsAppController.js
+++ courses/curso-javascript-completo/MD6-whatsapp/src/controller/WhatsAppController.js
@@ -9,6 +9,7 @@ import { Chat } from '../model/Chat';
 import { Message } from './../model/Message';
 import { Base64 } from '../util/base64';
 import { ContactsController } from './ContactsController';
+import { Upload } from '../util/Upload';
 
 export class WhatsAppController {
 
@@ -408,6 +409,24 @@ export class WhatsAppController {
 
         this.el.photoContainerEditProfile.on('click', e=>{
             this.el.inputProfilePhoto.click();
+        });
+
+        this.el.inputProfilePhoto.on('change', e=>{
+
+            if(this.el.inputProfilePhoto.files.length > 0){
+
+                let file = this.el.inputProfilePhoto.files[0];
+
+                Upload.send(file, this._user.email).then(snapshot=>{
+
+                    this._user.photo = snapshot;
+                    this._user.save();
+
+                });
+
+
+            }
+
         });
 
         this.el.inputNamePanelEditProfile.on('keypress', e=>{
