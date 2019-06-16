90a7b4fb58f9afeb8aad2df8b34e3a3418c9e48f
dcmunhoz <dc.munhoz@hotmail.com>
2019-06-16 18:58:46 -0300

    JS - MD07A02 - Criando estrutura do projeto

---
+++ courses/curso-javascript-completo/MD07-saboroso/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>  
+    <meta charset="utf-8">
+    <title>Restaurante Saboroso</title>
+    <style>
+        body{
+            font-family:'Helvetica';
+        }
+    </style>
+</head>
+<body>
+    
+    <h1>Restaurante Saboroso</h1>
+    <p>
+        Para visualizar este projeto, é necessário abrir o caminho via cmd e executar o comando ""
+    </p>
+
+
+</body>
+
+</html>
