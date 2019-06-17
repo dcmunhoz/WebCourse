accb772ee60e58760cab85e215407b6a507fb7f2
dcmunhoz <dc.munhoz@hotmail.com>
2019-06-16 21:06:44 -0300

    JS - MD07A07 - Organizando o HEADER com includes

--- courses/curso-javascript-completo/MD07-saboroso/routes/index.js
+++ courses/curso-javascript-completo/MD07-saboroso/routes/index.js
@@ -24,14 +24,18 @@ router.get('/', function(req, res, next) {
 router.get('/contacts', function(req, res, next){
 
   res.render('contacts', {
-    title: "Contatos"
+    title: "Contatos",
+    background: "images/img_bg_3.jpg",
+    h1: 'Diga um Oi!'
   });
 
 });
 
 router.get('/menus', function(req, res, next){
   res.render('menus', {
-    title: "Menus"
+    title: "Menus",
+    background: "images/img_bg_1.jpg",
+    h1: 'Saboreie nosso menu!'
   });
 
 });
@@ -39,7 +43,9 @@ router.get('/menus', function(req, res, next){
 router.get('/reservations', function(req, res, next){
   
   res.render('reservations', {
-    title: "Fazer uma reserva"
+    title: "Fazer uma reserva",
+    background: "images/img_bg_2.jpg",
+    h1: 'Reserve uma mesa!'
   });
 
 });
@@ -47,7 +53,9 @@ router.get('/reservations', function(req, res, next){
 router.get('/services', function(req, res, next){
 
   res.render("services", {
-    title: "Serviços disponiveis"
+    title: "Serviços disponiveis",
+    background: "images/img_bg_1.jpg",
+    h1: 'É um prazer poder servir!'
   });
 
 });
