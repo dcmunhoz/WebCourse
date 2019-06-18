d14435fbd922365910e563fb785b17e85b8332c8
dcmunhoz <dc.munhoz@hotmail.com>
2019-06-18 00:08:26 -0300

    JS - MD07A14 - Express session com redis

--- courses/curso-javascript-completo/MD07-saboroso/routes/admin.js
+++ courses/curso-javascript-completo/MD07-saboroso/routes/admin.js
@@ -10,6 +10,14 @@ router.get('/', function(req, res, next){
 
 router.get('/login', function(req, res, next){
 
+    if (!req.session.views){
+        
+        req.session.views = 0;
+
+    }
+    
+    console.log(req.session.views++);
+
     res.render('admin/login');
 
 });
