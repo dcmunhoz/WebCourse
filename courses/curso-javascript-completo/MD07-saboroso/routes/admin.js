58afdd8b8d73ad890ac8547263ec18ee9721ee3e
dcmunhoz <dc.munhoz@hotmail.com>
2019-06-18 14:32:22 -0300

    JS - MD07A18 - Links do menu

--- courses/curso-javascript-completo/MD07-saboroso/routes/admin.js
+++ courses/curso-javascript-completo/MD07-saboroso/routes/admin.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var users = require('./../includes/users');
+var admin = require('./../includes/admin');
 
 router.use(function(req, res, next){
 
@@ -14,6 +15,14 @@ router.use(function(req, res, next){
 
 });
 
+router.use(function(req, res, next){
+
+    req.menus = admin.getMenus();
+
+    next();
+
+});
+
 router.get('/logout', function(req, res, next){
 
     delete req.session.user;
@@ -23,7 +32,9 @@ router.get('/logout', function(req, res, next){
 
 router.get('/', function(req, res, next){
 
-    res.render("admin/index.ejs");
+    res.render("admin/index.ejs", {
+        menus: req.menus
+    });
 
 });
 
@@ -60,25 +71,32 @@ router.get('/login', function(req, res, next){
 
 router.get('/contacts', function(req, res, next){
 
-    res.render('admin/contacts');
+    res.render('admin/contacts', {
+        menus: req.menus
+    });
 
 });
 
 router.get('/emails', function(req, res, next){
 
-    res.render('admin/emails');
+    res.render('admin/emails', {
+        menus: req.menus
+    });
 
 });
 
 router.get('/menus', function(req, res, next){
 
-    res.render('admin/menus');
+    res.render('admin/menus', {
+        menus: req.menus
+    });
 
 });
 
 router.get('/reservations', function(req, res, next){
 
     res.render('admin/reservations', {
+        menus: req.menus,
         date: []
     });
 
@@ -86,7 +104,9 @@ router.get('/reservations', function(req, res, next){
 
 router.get('/users', function(req, res, next){
 
-    res.render('admin/users');
+    res.render('admin/users', {
+        menus: req.menus
+    });
 
 });
 
