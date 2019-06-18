79fbfff34d82ed968ac4385a1512f789a90f3434
dcmunhoz <dc.munhoz@hotmail.com>
2019-06-18 19:47:29 -0300

    JS - MS07A24 - ADM Menu enviando novo menu

--- courses/curso-javascript-completo/MD07-saboroso/routes/admin.js
+++ courses/curso-javascript-completo/MD07-saboroso/routes/admin.js
@@ -105,7 +105,15 @@ router.get('/menus', function(req, res, next){
 
 router.post('/menus', function(req, res, next){
 
-    res.send(req.body);
+    menus.save(req.fields, req.files).then(results=>{
+
+        window.location.reload();
+
+    }).catch(err=>{
+
+        res.send(err);
+
+    });
 
 });
 
