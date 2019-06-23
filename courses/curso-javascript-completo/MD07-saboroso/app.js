a634a9cfd5911efbc05b774b011630de9222573e
dcmunhoz <dc.munhoz@hotmail.com>
2019-06-23 14:56:49 -0300

    JS - MD07A49 - Instalando Socket.io

--- courses/curso-javascript-completo/MD07-saboroso/app.js
+++ courses/curso-javascript-completo/MD07-saboroso/app.js
@@ -7,11 +7,21 @@ var session = require('express-session');
 var RedisStore = require('connect-redis')(session);
 var formidable = require('formidable');
 var path = require('path');
+var http = require('http');
+var socket = require('socket.io');
 
 var indexRouter = require('./routes/index');
 var adminRouter = require('./routes/admin');
 
 var app = express();
+
+var http = http.Server(app);
+var io = socket(http);
+
+io.on('connection', function(socket){
+  console.log("usuário conectado");
+
+});
 
 app.use(function(req, res, next){
 
@@ -56,7 +66,7 @@ app.use(session({
 
 
 app.use(logger('dev'));
-app.use(express.json());
+//app.use(express.json());
 //app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -80,4 +90,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+http.listen(3000, function(){
+  console.log("Servidor em execução.");
+})
