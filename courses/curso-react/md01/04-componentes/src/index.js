3e1ac6cd339362a1e8ba4cd9d82a2f5185577287
dcmunhoz <dc.munhoz@hotmail.com>
2019-06-25 10:31:49 -0300

    RC - MD01A13 - Primeiro Componente,

---
+++ courses/curso-react/md01/04-componentes/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PrimeiroComponente from './components/PrimeiroComponente';
+
+const root = document.querySelector("#root");
+ReactDOM.render(
+    <div>
+        <PrimeiroComponente />
+    </div>  
+, root);
