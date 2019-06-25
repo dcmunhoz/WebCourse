cc0618dab162a8897edee203c7bea2327c013ff1
dcmunhoz <dc.munhoz@hotmail.com>
2019-06-25 12:17:08 -0300

    RC - MD01A19 - Relação entre componentes #1

---
+++ courses/curso-react/md01/08-relacao-componentes/src/components/FamiliaSilva.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Membro from './Membro';
+
+export default () => (
+
+    <div>
+        <h1>Familia Silva</h1>
+        <Membro nome="Daniel" sobrenome="silva"/>
+        <Membro nome="João" sobrenome="silva"/>
+        <Membro nome="Ana" sobrenome="silva"/>
+    </div>
+
+)
---
+++ courses/curso-react/md01/08-relacao-componentes/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Silva from './components/FamiliaSilva';
+
+ReactDOM.render(
+    <div>
+        <Silva />
+    </div>
+, document.querySelector("#root"));
