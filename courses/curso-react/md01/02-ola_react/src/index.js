8f1f9cf50d03ffb004c571de364723f4461b3434
dcmunhoz <dc.munhoz@hotmail.com>
2019-06-25 09:50:04 -0300

    RC - MD01A10 - Olá React

---
+++ courses/curso-react/md01/02-ola_react/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const elemento = document.querySelector("#root");
+ReactDOM.render(<h1>Olá, React</h1>, elemento);
+
+// const elemento = document.querySelector("#root");
+// ReactDOM.render("Olá, React", elemento);
