3102f6f423e4695b7c8f44e0ae55891d8f169306
dcmunhoz <dc.munhoz@hotmail.com>
2019-06-26 12:14:16 -0300

    RC - MD01A29 - Problema do "this"

--- courses/curso-react/md01/12-state-componente/src/components/Contador.jsx
+++ courses/curso-react/md01/12-state-componente/src/components/Contador.jsx
@@ -2,8 +2,26 @@ import React from 'react';
 
 export default class Contador extends React.Component{
 
-    maisUm(){
-        this.props.numero++;
+    // Solução 1. Bind no construtor
+    // constructor(props){
+
+    //     super(props);
+    //     this.maisUm = this.maisUm.bind(this);
+
+    // }
+
+    // Solução 2. Chama a função, dentro de uma arrow function
+    // <button onClick={() => this.maisUm() }>+</button>
+
+    // Solução 3. Cria a função como uma arrow function
+    // maisUm = () => {
+    //     // this.props.numero++;
+    //     console.log(this);
+    // }
+    
+    maisUm = () => {
+        // this.props.numero++;
+        console.log(this);
     }
 
     render() {
@@ -11,7 +29,7 @@ export default class Contador extends React.Component{
             <div>
 
                 <div>Número: {this.props.numero}</div>
-                <button onClick={this.maisUm }>+</button>
+                <button onClick={this.maisUm}>+</button>
                 <button>-</button>
 
             </div>
