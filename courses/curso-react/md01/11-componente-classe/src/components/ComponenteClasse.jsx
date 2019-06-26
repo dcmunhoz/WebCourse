86b624cc3ebb617045d5dcc8322239dc5e9cfa19
dcmunhoz <dc.munhoz@hotmail.com>
2019-06-26 11:38:06 -0300

    RC - MD01A26 - Componente Classe

---
+++ courses/curso-react/md01/11-componente-classe/src/components/ComponenteClasse.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class ComponenteClasse extends React.Component{
+
+    render() {
+        return (
+            <h1>{this.props.value || 'Valor padrão'}</h1>
+        )
+    }
+
+}
---
+++ courses/curso-react/md01/11-componente-classe/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Classe from './components/ComponenteClasse';
+
+ReactDOM.render(
+    <Classe value="Primeiro componente classe"/>
+, document.querySelector("#root"));
