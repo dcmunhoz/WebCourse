6caf2f14427c88841da814249e3b390a90f05c3a
dcmunhoz <dc.munhoz@hotmail.com>
2019-06-26 11:24:25 -0300

    RC - MD01A25 - Comunicação indireta entre componentes

---
+++ courses/curso-react/md01/10-comunicacao-indireta/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pai from './components/Pai';
+
+ReactDOM.render(
+    <div>
+        <Pai />
+    </div>
+, document.querySelector("#root"));
