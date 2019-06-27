6952d04f3e93f600260d343e98cb300fd3402661
dcmunhoz <dc.munhoz@hotmail.com>
2019-06-27 11:29:14 -0300

    RC - MD02A35 - Criando projeto/Componente Calculator

---
+++ courses/curso-react/md02/01-calculadora/src/main/Calculator.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Calculator.css';
+
+export default class Calculator extends React.Component {
+
+    render() {
+        return (
+            <div className="calculator">
+
+            </div>
+        )
+    }
+
+}
