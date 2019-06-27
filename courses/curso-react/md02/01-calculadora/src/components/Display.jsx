5f5fea5abc83e47cf3eb585a3af36f3075071dd7
dcmunhoz <dc.munhoz@hotmail.com>
2019-06-27 11:53:26 -0300

    RC - MD02A37 - Componente Display

---
+++ courses/curso-react/md02/01-calculadora/src/components/Display.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Display.css';
+
+export default props=>(
+    <div className="display">{props.value}</div>
+
+)
+
+// export default class Display extends React.Component{
+
+
+
+// }
