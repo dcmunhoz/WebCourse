846d146c498f70f4ec1c6a9a44050d76a9ed8802
dcmunhoz <dc.munhoz@hotmail.com>
2019-06-27 11:41:24 -0300

    RC - MD02A36 - Componente button

---
+++ courses/curso-react/md02/01-calculadora/src/components/Button.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Button.css'
+
+export default props => (
+
+    <button className="button">{props.label}</button>
+
+)
+
+// export default class Button extends React.Component {
+
+//     render(){
+//         return(
+            
+//         )
+//     }
+
+// }
