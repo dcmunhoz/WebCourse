332b8151d0725529c459d567fc97fd0490105faf
dcmunhoz <dc.munhoz@hotmail.com>
2019-07-02 14:33:17 -0300

    RC - MD03 - Adicionar teclas de atalho

--- courses/curso-react/md03/todo-frontend/src/todo/TodoForm.jsx
+++ courses/curso-react/md03/todo-frontend/src/todo/TodoForm.jsx
@@ -2,17 +2,29 @@ import React from 'react';
 import Grid from './../template/grid';
 import IconButton from '../template/iconButton';
 
-export default props=>(
-    <div role="form" className="todoForm" >
-        <Grid cols="12 9 10">
-            <input onChange={props.handleChange} value={props.description} type="text" id="description" className="form-control" placeholder="adicione uma tarefa" />
-        </Grid>
-        
-        <Grid cols="12 3 2">
-            <IconButton style="primary" icon="plus" onClick={props.handleAdd}></IconButton>
-            <IconButton style="info" icon="search" onClick={props.handleSearch} />
-            <IconButton style="default" icon="close" onClick={props.handleClear} />
-        </Grid>
-    </div>
+export default props=>{
 
-)
+    const keyHandler = (e) =>{
+        if (e.key === 'Enter') {
+            e.shiftKey ? props.handleSearch() : props.handleAdd()
+        }else if (e.key==="Escape"){
+            props.handleClear();
+        }
+    }
+
+    return(
+        <div role="form" className="todoForm" >
+            <Grid cols="12 9 10">
+                <input onKeyUp={keyHandler} onChange={props.handleChange} value={props.description} type="text" id="description" className="form-control" placeholder="adicione uma tarefa" />
+            </Grid>
+            
+            <Grid cols="12 3 2">
+                <IconButton style="primary" icon="plus" onClick={props.handleAdd}></IconButton>
+                <IconButton style="info" icon="search" onClick={props.handleSearch} />
+                <IconButton style="default" icon="close" onClick={props.handleClear} />
+            </Grid>
+        </div>
+    
+    )
+
+}
